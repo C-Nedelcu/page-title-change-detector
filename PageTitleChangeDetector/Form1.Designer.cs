464791a11a2013e5908b98143e30b0b3040a5a91
dev-eduka <c.nedelcu@edukasoftware.com>
2021-06-10 15:53:12 +0800

    1.0.0
    Initial release

---
+++ PageTitleChangeDetector/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace PageTitleChangeDetector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(160, 29);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(406, 27);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Page URL:";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Refresh every:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(160, 64);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 27);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(286, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "seconds";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Current status:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(156, 102);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(175, 20);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Idle - press start to begin";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 136);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(117, 20);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Initial page title:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(157, 139);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(14, 17);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "-";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(12, 171);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(128, 20);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Current page title:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label9.Location = new System.Drawing.Point(157, 174);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(14, 17);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "-";
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(160, 217);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(198, 35);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(364, 217);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(198, 35);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(578, 272);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Page title change detector";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ PageTitleChangeDetector/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace PageTitleChangeDetector
+{
+    public partial class Form1 : Form
+    {
+        string CURRENT_URI = "";
+        string CURRENT_STATUS = "idle";
+        int CHECK_COUNT = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public bool UpdateInitialTitle()
+        {
+            var title = this.GetCurrentTitle();
+            if (title == "")
+            {
+                label7.Text = "Error: the page title could not be retrieved";
+                label7.ForeColor = Color.Red;
+                return false;
+            }
+            label7.ForeColor = SystemColors.ControlText;
+            label7.Text = title;
+            return true;
+        }
+
+        public bool VerifyTitleChanged()
+        {
+            // redownload page, check title changed or not
+            var newTitle = this.GetCurrentTitle();
+            var oldTitle = label7.Text;
+            if (newTitle == "")
+            {
+                label9.Text = "Error: the page title could not be retrieved";
+                label9.ForeColor = Color.Red;
+                label9.Font = new Font(label9.Font, FontStyle.Regular);
+                label9.Refresh();
+                return false;
+            }
+            else if (newTitle == oldTitle) { 
+                label9.ForeColor = SystemColors.ControlText;
+                label9.Font = new Font(label9.Font, FontStyle.Regular);
+                label9.Text = newTitle;
+                label9.Refresh();
+                return false;
+            }
+            else {
+                // Changed!
+                label9.ForeColor = SystemColors.Highlight;
+                label9.Text = newTitle;
+                label9.Font = new Font(label9.Font, FontStyle.Bold);
+                label9.Refresh();
+                return true;
+            }
+        }
+
+        public string GetCurrentTitle()
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            WebClient x = new WebClient();
+            x.Encoding = Encoding.UTF8;
+
+            // Get source & parse text
+            try
+            {
+                x.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36");
+                string source = x.DownloadString(CURRENT_URI);
+                string title = Regex.Match(source, @"\<title\b[^>]*\>\s*(?<Title>[\s\S]*?)\</title\>",
+                    RegexOptions.IgnoreCase).Groups["Title"].Value;
+                return title;
+            }
+            catch (WebException ex)
+            {
+                var resp = (HttpWebResponse)ex.Response;
+                if (resp != null && resp.StatusCode == HttpStatusCode.NotFound) // HTTP 404
+                {
+                    //the page was not found, continue with next in the for loop
+                    return "404 - Not Found";
+                }
+                button2_Click(null, null);
+                MessageBox.Show("Error: "+ex.Message);
+                return "";
+            }
+            catch (Exception ex)
+            {
+                button2_Click(null, null);
+                MessageBox.Show("Error: " + ex.Message);
+                return "";
+            }
+        }
+
+        public void UpdateStatus()
+        {
+            if (this.CURRENT_STATUS == "initial")
+            {
+                label5.Text = "Getting initial title...";
+            }
+            else if (this.CURRENT_STATUS == "timer")
+            {
+                label5.Text = "Waiting for next verification (#"+this.CHECK_COUNT+")...";
+            }
+            else 
+            {
+                label5.Text = "Idle - press start to begin";
+            }
+
+            label5.Refresh();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Validate URI
+            this.CHECK_COUNT = 1;
+            var uriName = textBox1.Text;
+            Uri uriResult;
+            bool result = Uri.TryCreate(uriName, UriKind.Absolute, out uriResult)
+                && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+            if (!result)
+            {
+                MessageBox.Show("The specified URL does not seem to be valid.");
+                return;
+            }
+            this.CURRENT_URI = uriName;
+
+            // Update status
+            CURRENT_STATUS = "initial";
+            this.UpdateStatus();
+
+            // Try updating the initial title
+            label9.Text = "";
+            if (!UpdateInitialTitle()) return;
+
+            // Switch button state
+            button1.Enabled = false;
+            button2.Enabled = true;
+            textBox1.Enabled = false;
+            numericUpDown1.Enabled = false;
+
+            // Start the timers
+            timer1.Interval = Convert.ToInt32(numericUpDown1.Value) * 1000;
+            timer1.Start(); // Start the timer that will perform the check
+
+            // Update status
+            CURRENT_STATUS = "timer";
+            this.UpdateStatus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Switch button state
+            button1.Enabled = true;
+            button2.Enabled = false;
+            textBox1.Enabled = true;
+            numericUpDown1.Enabled = true;
+
+            // Stop timer
+            timer1.Stop();
+
+            // Update status
+            CURRENT_STATUS = "idle";
+            this.UpdateStatus();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (this.VerifyTitleChanged())
+            {
+                // Changed! press Stop, and then alert
+                button2_Click(sender, e);
+
+                // BeEp
+                System.Media.SystemSounds.Beep.Play();
+                System.Media.SystemSounds.Beep.Play();
+                System.Media.SystemSounds.Beep.Play();
+                MessageBox.Show("The page title has changed ("+ DateTime.Now.ToString("dddd, dd MMMM yyyy HH:mm:ss")+")\n\nThe new title is:\n"+label9.Text);
+            }
+            else
+            {
+                // No change
+                this.CHECK_COUNT++;
+                this.UpdateStatus();
+            }
+
+        }
+    }
+}
